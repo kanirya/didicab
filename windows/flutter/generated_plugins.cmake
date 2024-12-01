261eefa51207d1e4eb6808e6f1136a2d8f6c98ed
Muhammad <codewithartist1@gmail.com>
2024-12-01 21:08:37 +0500

    UPloaded

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,14 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  file_selector_windows
+  firebase_auth
+  firebase_core
+  firebase_storage
+  geolocator_windows
+  permission_handler_windows
+  smart_auth
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
